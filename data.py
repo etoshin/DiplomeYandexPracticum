e8171cfec2857ace9fc125b0b012ddd4ca261a35
etoshin <secci1337@gmail.com>
2024-07-20 15:23:58 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,17 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+order_body = {
+    "firstName": "Абдурахмангаджи",
+    "lastName": "Убдурахмангаджи",
+    "address": "Центральный проезд Хорошёвского Серебряного Бора 2",
+    "metroStation": 204,
+    "phone": "+34916123451",
+    "rentTime": 5,
+    "deliveryDate": "2024-07-17",
+    "comment": "Привет, Абдурахмангаджи!",
+    "color": [
+
+    ]
+}
